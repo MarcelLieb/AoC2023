9dbda221b2f19c7cce9a6981f40d4cf95b3c9663
Marcel Lieb <marclie1@gmx.de>
2023-12-04 13:31:17 +0100

    implemented DefaultHashMap

--- aoc2023-4/src/main.rs
+++ aoc2023-4/src/main.rs
@@ -1,4 +1,5 @@
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fs, ops::{DerefMut, Deref, Index}};
+use std::hash::Hash;
 
 fn main() {
     let input: Vec<String> = fs::read_to_string("./input.txt")
@@ -37,26 +38,22 @@ fn main() {
         })
         .collect();
 
-    let mut counter: HashMap<usize, u64> = HashMap::new();
+    let mut counter: DefaultHashMap<usize, u32> = DefaultHashMap::new(1_u32);
 
     for (i, wins) in winnings.iter().enumerate() {
-        counter.entry(i).or_insert(1);
-        let multiplier = counter[&i];
+        let multiplier = *(counter.get_default(i));
         for &number in wins.iter() {
-            let count = counter.entry(number).or_insert(1);
-            *count = *count + multiplier;
+            *counter.entry_default(number) += multiplier;
         }
     }
-    let part_two: u64 = counter.values().sum();
+    let part_two: u32 = counter.values().sum();
 
     println!("{}", part_two);
 }
 
 fn parse_card(card: &str) -> (u32, Vec<u32>, Vec<u32>) {
     let (card, numbers) = card.split_once(": ").unwrap();
-    let mut card = card.split_whitespace();
-    card.next();
-    let card: u32 = card.next().unwrap().parse().unwrap();
+    let card: u32 = card.split_whitespace().nth(1).unwrap().parse().unwrap();
 
     let (winning, ticket) = numbers.split_once("| ").unwrap();
 
@@ -71,4 +68,52 @@ fn parse_card(card: &str) -> (u32, Vec<u32>, Vec<u32>) {
         .collect();
 
     (card, winning, numbers)
+}
+
+struct DefaultHashMap<K : Eq + PartialEq + Hash, V: Clone> {
+    inner: HashMap<K, V>,
+    default_value: V,
+}
+
+impl<K: Eq + PartialEq + Hash,V: Clone> DefaultHashMap<K, V> {
+    fn new(default: V) -> DefaultHashMap<K, V>{
+        DefaultHashMap{inner: HashMap::new(), default_value: default}
+    }
+
+    fn entry_default(&mut self, key: K) -> &mut V{
+        let default = self.default_value.clone();
+        self.entry(key).or_insert(default)
+    }
+
+    fn get_default(&mut self, key: K) -> &V {
+        let default = self.default_value.clone();
+        self.entry(key).or_insert(default)
+    }
+}
+
+impl <K: Eq + PartialEq + Hash, V: Clone> Index<K> for DefaultHashMap<K, V> {
+    type Output = V;
+    /*
+    Don't use this if you want the entry to be added
+     */
+    fn index(&self, index: K) -> &Self::Output {
+        match self.inner.get(&index) {
+            Some(value) => value,
+            None => &self.default_value
+        }
+    }
+}
+
+impl<K: Eq + PartialEq + Hash, V: Clone> Deref for DefaultHashMap<K, V> {
+    type Target = HashMap<K, V>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl<K: Eq + PartialEq + Hash, V: Clone> DerefMut for DefaultHashMap<K, V> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
 }
